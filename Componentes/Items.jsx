65b79050e802f691b9d4374c83e13532431e9eb9
JorgeMedranoNovas <95459237+JorgeMedranoNovas@users.noreply.github.com>
2022-03-21 20:22:58 -0400

    Add files via upload

--- Componentes/Items.jsx
+++ Componentes/Items.jsx
@@ -6,21 +6,7 @@ const Items = ({items}) => {
     <>
     {
       items.map((item) => (
-        <div className="col-sm-10" key={item.id}>
-        <p>{item.nombre}</p>
-        <div className="row item">
-            <div className="col-sm-3">
-                
-            </div>
-            <div className="col-sm-8">
-               {item.apellido}
-            </div>
-            <div className="col-sm-1">
-               {item.telefono}
-            </div>
-        </div>
-      </div> 
-       // <Item item={item} />     
+        <Item item={item} />     
       ))
     }
     </>
